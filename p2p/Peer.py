605266317a0219735a81d7eef3b1d70a3a72f003
taimurt <taimurt@gmail.com>
2016-06-09 15:58:28 +0300

    Update Peer.py

--- p2p/Peer.py
+++ p2p/Peer.py
@@ -1,3 +1,5 @@
+#This is a UDP based P2P Chat Application developed by Taimur Tufail, Tiziano Contaldo & Kristen Kivimaa
+
 class Peer(object):
     def __init__(self, addr,  id):
         self.address = addr

d149158be3a69ec34bb86cfa655c57b700d5d055
taimurt <taimurt@gmail.com>
2016-06-09 15:59:04 +0300

    Update User.py

--- p2p/User.py
+++ p2p/User.py
@@ -1,3 +1,5 @@
+#This is a UDP based P2P Chat Application developed by Taimur Tufail, Tiziano Contaldo & Kristen Kivimaa
+
 class User(object):
     def __init__(self, uuid, port):
         self.uuid=uuid

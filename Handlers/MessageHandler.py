975fbb6e48a423166c82bf38f2425e35be1576a6
taimurt <taimurt@gmail.com>
2016-06-09 16:18:04 +0300

    Update MessageHandler.py

--- Handlers/MessageHandler.py
+++ Handlers/MessageHandler.py
@@ -1,3 +1,5 @@
+#This is a UDP based P2P Chat Application developed by Taimur Tufail, Tiziano Contaldo & Kristen Kivimaa
+
 #from p2p.Handlers.Handler import Handler
 from p2p.Handlers import *
 from p2p.Flag import *
